17357e9e473e5a3a85ebd4200a5429445a999530
Aslam <apps@hotdogcode.com>
2024-02-22 11:53:54 +0530

    implement repository and use case for single weather response

--- domain/src/main/java/com/hdc/domain/weather/usecase/GetWeatherUseCase.kt
+++ domain/src/main/java/com/hdc/domain/weather/usecase/GetWeatherUseCase.kt
@@ -1,8 +1,17 @@
 package com.hdc.domain.weather.usecase
 
+import com.hdc.domain.architecture.coroutine.CoroutineContextProvider
+import com.hdc.domain.architecture.usecase.BackgroundExecutingUseCase
+import com.hdc.domain.weather.model.WeatherRequestDomainModel
+import com.hdc.domain.weather.model.WeatherResponseDomainModel
 import com.hdc.domain.weather.repository.GetWeatherRepository
 
 
 class GetWeatherUseCase(
-    private val getWeatherRepository: GetWeatherRepository
-)
+    private val getWeatherRepository: GetWeatherRepository,
+    coroutineContextProvider: CoroutineContextProvider
+):BackgroundExecutingUseCase<WeatherRequestDomainModel,WeatherResponseDomainModel>(coroutineContextProvider){
+    override fun executeInBack(request: WeatherRequestDomainModel) =
+        getWeatherRepository.getWeather(request)
+
+}
